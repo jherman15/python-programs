87b5036bfbebc538e1e58e8bcfb76828853100ea
Jaroslaw Herman <jherman@student.agh.edu.pl>
2022-11-06 14:39:18 +0100

    improved readability

--- iloczyn_skalarny.py
+++ iloczyn_skalarny.py
@@ -1,6 +1,9 @@
 import numpy as np
 
-a = [1, 2, 12, 4]
-b = [2, 4, 2, 8]
+array1 = [1, 2, 12, 4]
+array2 = [2, 4, 2, 8]
 
-print(np.dot(a, b))
+print('\nArray1: ', array1)
+print('Array2: ', array2)
+
+print('\nDot product of the given arrays: ', np.dot(array1, array2))
