72124db0038f8091e3dc34c5ae47d7c9357a850c
jherman15 <jherman@student.agh.edu.pl>
2022-12-05 19:20:02 +0100

    v2

--- zrownoleglanie_obliczen/pieciu_filozofow.py
+++ zrownoleglanie_obliczen/pieciu_filozofow.py
@@ -1,33 +1,37 @@
+import random
 import threading
+import time
 
-class DiningPhilosophers:
-    def __init__(self):
-        # List of semaphores, one for each fork
-        self.forks = [threading.Semaphore(1) for _ in range(5)]
 
-    # call the functions directly to execute, for example, eat()
-    def wantsToEat(self,
-                   philosopher: int,
-                   pickLeftFork: 'Callable[[], None]',
-                   pickRightFork: 'Callable[[], None]',
-                   eat: 'Callable[[], None]',
-                   putLeftFork: 'Callable[[], None]',
-                   putRightFork: 'Callable[[], None]') -> None:
-        # Take the right fork, then left fork
-        self.forks[philosopher].acquire()
-        self.forks[(philosopher + 1) % 5].acquire()
+class Philosopher(threading.Thread):
 
-        # Eat!
-        pickRightFork() 
-        pickLeftFork()
-        eat()
-        putRightFork()
-        putLeftFork()
+    def __init__(self, philosopher_num, left_fork, right_fork):
+        super().__init__()  # multiple inheritance
+        self.philosopher_num = philosopher_num
+        self.left_fork = left_fork
+        self.right_fork = right_fork
 
-        # Release right fork, then left fork
-        self.forks[philosopher].release()
-        self.forks[(philosopher + 1) % 5].release()
+    def try_to_eat(self):
+        self.left_fork.acquire()
+        self.right_fork.acquire()
 
-        # widelce i filozofow
-        # filozof metoda widelec i nie musi sprawdzac, czy to moze
-        # jezeli nie moze, to ma być wyjątek i filozof nie może wywołać metody
+        print(f"Philosopher {self.philosopher_num} is eating")
+        time.sleep(5)
+        print(f"Philosopher {self.philosopher_num} stopped eating")
+
+        self.left_fork.release()
+        self.right_fork.release()
+
+    def run(self):
+        while True:
+            print(f"Philosopher {self.philosopher_num} is thinking")
+            time.sleep(random.randint(1, 6))
+            self.try_to_eat()
+
+
+if __name__ == '__main__':
+    forks = [threading.Condition(threading.Lock()) for index in range(5)]
+    philosophers = [Philosopher(index, forks[index], forks[(index + 1) % 5]) for index in range(5)]
+
+    for p in philosophers:
+        p.start()
